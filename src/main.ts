c53bece4a3a145f2b382384539b51dc45f9afc55
h3amdy <hamd4y4@gmail.com>
2026-03-03 00:29:22 +0300

    زيادة حجم ال body لأجل الوسائط

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    rawBody: true,
+  });
+
+  // ✅ زيادة حد حجم الـ body لدعم رفع chunks كبيرة (حتى 25MB)
+  app.useBodyParser('json', { limit: '5mb' });
+  app.useBodyParser('raw', { limit: '25mb' });
 
   // ✅ تفعيل CORS عشان Flutter (وحتى لو صار عندك Web)
   app.enableCors({
